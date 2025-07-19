4166bf6c974229c5fcd06d5838c0ecf88e2688ad
TAMAKA JAGJEEVANRAM <jagjeevanram417@gmail.com>
2025-07-19 22:49:43 +0530

    Time: 10 ms (87.21%) | Memory: 20.6 MB (76.54%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram=defaultdict(list)
+        for i in strs:
+            key=''.join(sorted(i))
+            anagram[key].append(i)
+        return list(anagram.values())    
